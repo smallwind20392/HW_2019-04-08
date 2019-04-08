90e3fabbd9ed1362ee95c25334f4d8e519bce41d
smallwind20392 <49222874+smallwind20392@users.noreply.github.com>
2019-04-08 17:02:04 +0800

    first commit

---
+++ mi_to_km.java
@@ -0,0 +1,12 @@
+/**
+ * mi_to_km
+ */
+import java.util.*;
+public class mi_to_km {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float c = input.nextFloat();
+        System.out.println(c * (62137/100000f));
+    
+}
+}
