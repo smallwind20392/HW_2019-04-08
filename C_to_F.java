10fd1bdc6ebbfda2ca6bfd7fbc84374a9995d17e
smallwind20392 <49222874+smallwind20392@users.noreply.github.com>
2019-04-08 17:21:32 +0800

    first commit

---
+++ C_to_F.java
@@ -0,0 +1,11 @@
+/**
+ * C_to_F
+ */
+import java.util.*;
+public class C_to_F {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        float c = scn.nextFloat();
+        System.out.println(c * (9f/5f)+32 );
+    }
+}
