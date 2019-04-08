dfa8cbe4aaaf4c90484df97f9a89a1a331fc770a
smallwind20392 <49222874+smallwind20392@users.noreply.github.com>
2019-04-08 17:24:00 +0800

    first commit

---
+++ c_m_p.java
@@ -0,0 +1,19 @@
+/**
+ * c_m_p
+ */
+import java.util.*;
+public class c_m_p {
+    public static void main(String[] args) {
+        Scanner input_1 = new Scanner(System.in);
+        Scanner input_2 = new Scanner(System.in);
+        int a = input_1.nextInt();
+        int b = input_2.nextInt();
+        System.out.print("Combine = ");
+        System.out.println(a + b);
+        System.out.print("Minus   = ");
+        System.out.println(a-b);
+        System.out.print("product = ");
+        System.out.println(a*b);
+}
+    
+}
