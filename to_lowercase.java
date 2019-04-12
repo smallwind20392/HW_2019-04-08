9aa8a82b7473993ceb9fe48feb87f1f7492c2fe2
smallwind20392 <49222874+smallwind20392@users.noreply.github.com>
2019-04-12 15:59:04 +0800

    init commit

---
+++ to_lowercase.java
@@ -0,0 +1,12 @@
+/**
+ * to_lowercase
+ */
+import java.util.*;
+public class to_lowercase {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        System.out.println(str.toLowerCase());
+    }
+}
