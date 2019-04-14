2bf5f2edcd1114003b2349fbc4883bdb229e8cb2
smallwind20392 <49222874+smallwind20392@users.noreply.github.com>
2019-04-14 22:59:07 +0800

    first commit

---
+++ g_to_jacket.java
@@ -0,0 +1,12 @@
+/**
+ * g_to_jacket
+ */
+import java.util.*;
+public class g_to_jacket {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        float g = scn.nextFloat();
+        System.out.println(g * (1f/600f));
+    }
+}
