6f53616a2523068eb5b71e84f73598a3c32d240d
smallwind20392 <49222874+smallwind20392@users.noreply.github.com>
2019-04-14 23:10:43 +0800

    first commit

---
+++ char_replace.java
@@ -0,0 +1,11 @@
+/**
+ * char_replace
+ */
+import java.util.*;
+public class char_replace {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String enter = scn.nextLine();
+        System.out.println(enter.replace("i", "1"));
+    }
+}
