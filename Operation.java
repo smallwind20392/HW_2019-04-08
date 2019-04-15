a142674d310be48c098351e158af3ed99b23a04e
smallwind20392 <49222874+smallwind20392@users.noreply.github.com>
2019-04-15 14:30:18 +0800

    first commit

---
+++ Operation.java
@@ -0,0 +1,15 @@
+/**
+ * Operation
+ */
+import java.util.*;
+public class Operation {
+    public static void main(String[] args) {
+        Scanner input_1 = new Scanner(System.in);
+        int a = input_1.nextInt();
+        int b = input_1.nextInt();
+        System.out.println(a+"+"+b+"="+(a+b));
+        System.out.println(a+"-"+b+"="+(a-b));
+        System.out.println(a+"*"+b+"="+(a*b));
+        System.out.println(a+"%"+b+"="+(a%b));        
+}
+}
