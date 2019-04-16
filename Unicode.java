5041c604c261217a1c6eb14ee9ef04e65fdf2822
smallwind20392 <49222874+smallwind20392@users.noreply.github.com>
2019-04-16 08:10:06 +0800

    first commit

---
+++ Unicode.java
@@ -0,0 +1,13 @@
+/**
+ * Unicode
+ */
+import java.util.*;
+public class Unicode {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String ch = scn.next();
+        int ch_num = ch.charAt(0);
+        System.out.println(ch_num);
+    }
+}
