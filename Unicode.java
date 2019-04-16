9283ce01476b2b37d0e531c8dc6f46686d893a97
smallwind20392 <49222874+smallwind20392@users.noreply.github.com>
2019-04-16 10:38:26 +0800

    first commit

--- Unicode.java
+++ Unicode.java
@@ -5,9 +5,9 @@ import java.util.*;
 public class Unicode {
 
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        String ch = scn.next();
-        int ch_num = ch.charAt(0);
-        System.out.println(ch_num);
+		Scanner scn = new Scanner(System.in);
+        String ch = scn.nextLine();
+        char ch_num = ch.charAt(0);
+        System.out.println(Integer.toHexString((int)ch_num));
     }
 }
